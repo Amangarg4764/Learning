79b5f29109922c219a2b0376c7046d4a11aab477
Amangarg4764 <aman0944.cse19@chitkara.edu.in>
2023-06-16 22:48:05 +0530

    add consuption file

---
+++ ConsuptionRate.js
@@ -0,0 +1,23 @@
+const client = require("./Config/influx");
+const moment = require("moment");
+//function for caluclate Consumption rate between end and start date
+const findConsumption = async function (start, end, measurement, sensor) {
+  try {
+    //start value
+    const squery = `SELECT * FROM ${measurement} WHERE "sensor"='${sensor}' AND time >= ${start} limit 1`;
+    var svalue = await client.query(squery);
+    svalue = parseInt(svalue[0].value);
+    //end value
+    const equery = `SELECT * FROM ${measurement} WHERE "sensor" = '${sensor}' AND time <= '${end}' ORDER BY DESC LIMIT 1`;
+    var evalue = await client.query(equery);
+    evalue = parseInt(evalue[0].value);
+    //output
+    console.log(evalue - svalue);
+  } catch (err) {
+    console.log("error");
+  }
+};
+//Input
+var start = moment("2023-06-16 00:00:00").valueOf();
+var end = moment("2023-06-17 00:00:00").format();
+findConsumption(start, end, "Consumption", "RSSI");
--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const client=require('./Config/influx')
 const ExcelJS=require('exceljs');
+const moment=require('moment');
 //  --------------------------------------------------      CRUD operation-----------
 
 //influx fetch data ==> read data
 const fetchdata=async function(){
     try{
-      
+        
         const data=await client.query(`SELECT * FROM Consumption`);
         //code of excel workbook=file
         const Workbook=new ExcelJS.Workbook();
         //worksheet
         const WorkSheet=Workbook.addWorksheet('Consumption');
         WorkSheet.columns=[
-          //  {header:'s.no',key:'s.no',width:'10'},
-            {header:'time',key:'time',width:'30'},
-            {header:'device',key:'device',width:'20'},
-            {header:'sensor',key:'sensor',width:'10'},
-            {header:'value',key:'value',width:'10'}
+            //{header:'s.no',key:'s.no',width:'10'},
+            {header:'Time',key:'time',width:'30',style:{numFmt:'yyyy-mm-dd hh:mm:ss'}},
+            {header:'Device',key:'device',width:'20'},
+            {header:'Sensor',key:'sensor',width:'10'},
+            {header:'Value',key:'value',width:'10'}
         ]
         //add data to excel
         data.map(val=>{
             WorkSheet.addRow(val);
         });
-        const r=await Workbook.xlsx.writeFile('Consumption.xlsx');
-        console.log('done')
+        WorkSheet.getRow(1).eachCell(cell=>{
+            cell.font={bold:true}
+        })
+        //add workbook
+        await Workbook.xlsx.writeFile('Consumption.xlsx');
     }catch(err){
         console.log(err);
     }
@@ -38,6 +42,7 @@ module.exports.insertdata=async function(obj){
             for (let index = 0; index < obj.data.length; index++) {
                 
                 const element = obj.data[index];
+                
                 await client.writePoints([{
                 measurement:"Consumption",
                 time:obj.time,
